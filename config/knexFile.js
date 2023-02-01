e37e2149e7e96760f3e137c3f725b1fb0388714e
Ger1967 <german.alfaro.diaz@gmail.com>
2023-01-31 21:42:54 -0300

    inicio backend

---
+++ config/knexFile.js
@@ -0,0 +1,10 @@
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "postgres",
+    database: "deportistas",
+  },
+});
---
+++ controllers/index.js
@@ -0,0 +1,4 @@
+const knex = require("../config/knexFile");
+// const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
+// const { TOKEN_SECRET } = require("../validators/jwt");

94c31e9ae081e8d2009f5ff3cb0902b50be6bb39
sabirnamato <sabir.mato00@gmail.com>
2023-02-06 19:35:46 -0300

    ss

--- controllers/index.js
+++ controllers/index.js
@@ -23,13 +23,44 @@ exports.listEstadisticas = (req, res) => {
     });
 };
 
-//com
 exports.listEstadisticasById = (req, res) => {
   const id = req.params.id;
   knex("estadisticas")
     .where("id", id)
     .then((resultado) => {
       res.json(resultado);
+    })
+    .catch((error) => {
+      res.status(400).json({ error: error.message });
+    });
+};
+
+exports.register = async (req, res) => {
+  const {
+    nombre,
+    especialidad,
+    edad,
+    altura,
+    peso,
+    nacionalidad,
+    record,
+    descripcion,
+    // idestadisticas,
+  } = req.body;
+  knex("deportistas")
+    .insert({
+      nombre: nombre,
+      especialidad: especialidad,
+      edad: edad,
+      altura: altura,
+      peso: peso,
+      nacionalidad: nacionalidad,
+      record: record,
+      descripcion: descripcion,
+      // idestadisticas: idestadisticas,
+    })
+    .then((resultado) => {
+      res.json({ messagee: "Se ha registrado un nuevo deportista" });
     })
     .catch((error) => {
       res.status(400).json({ error: error.message });
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const {
   list,
   listEstadisticas,
   listEstadisticasById,
+  register,
 } = require("../controllers/index");
 
 router.get("/deportistas", list);
 router.get("/estadisticas", listEstadisticas);
 router.get("/estadisticas/:id", listEstadisticasById);
+router.post("/deportistas/register", register);
 
 module.exports = router;
